ae7cb87e550b5d4036d6e100e21fa55a8f258d4a
Lokendra Tyagi <lokendra.tyagi@egovernments.org>
2023-07-05 15:24:11 +0530

    added apiservice files

---
+++ src/main/java/com/springBoot/Postgres/AddressApiService.java
@@ -0,0 +1,46 @@
+package com.springBoot.Postgres;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.Map;
+
+@Service
+public class AddressApiService {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public String getAddressFromApi() {
+        String url = "https://random-data-api.com/api/v2/users?size=1";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
+        Map<String, Object> data = response.getBody();
+        Map<String, Object> addressData = (Map<String, Object>) data.get("address");
+        Address address = new Address();
+        address.setCity((String) addressData.get("city"));
+        address.setStreetName((String) addressData.get("street_name"));
+        address.setStreetAddress((String) addressData.get("street_address"));
+        address.setZipCode((String) addressData.get("zip_code"));
+        address.setState((String) addressData.get("state"));
+        address.setCountry((String) addressData.get("country"));
+        Map<String, Object> coordinatesData = (Map<String, Object>) addressData.get("coordinates");
+        Coordinates coordinates = new Coordinates();
+        coordinates.setLat((Double) coordinatesData.get("lat"));
+        coordinates.setLng((Double) coordinatesData.get("lng"));
+        address.setCoordinates(coordinates);
+
+        String addressJson;
+        try {
+            addressJson = objectMapper.writeValueAsString(address);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error serializing address object to JSON", e);
+        }
+
+        return addressJson;
+    }
+}
